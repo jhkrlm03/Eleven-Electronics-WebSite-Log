0b746349e1ea125596dd6e4487c108b06eb47389
jhkrlm03 <santiagogesualdi@gmail.com>
2023-01-19 17:03:31 -0300

    Eleven Electronics Web 2.0
    Re versión del código pasado mediante la incorporación de objetos, arrays y métodos de los mismo, desglosamiento en módulos para una mejor lectura de los mismo, entre otras incorporaciones. La entrega busca resolver las consignas planteadas y (por la no obligatoriedad del mismo) no cuenta con una estructura HTML / CSS detallada

--- PreEntrega 1 Gesualdi/js/script.js
+++ PreEntrega 1 Gesualdi/js/script.js
@@ -1,270 +1,365 @@
 const IVA = 21; //Impuesto al Valor Agregado
 
-
-const addIVA = (PriceWithOutIVA) => PriceWithOutIVA + PriceWithOutIVA * IVA / 100; //Agrega el IVA
+const addIVA = (PriceWithOutIVA) =>
+  PriceWithOutIVA + (PriceWithOutIVA * IVA) / 100; //Agrega el IVA
 
 const addDiscount = (PriceWithIVA) => PriceWithIVA - PriceWithIVA * discount; //Aplica el descuento
 
-const datoInvalido = () => alert('EL DATO INGRESADO NO FUE VALIDO'); 
+const datoInvalido = () => alert("EL DATO INGRESADO NO FUE VALIDO");
 
 //Precio de las gráficas
-const GTX_1660 = addIVA(120000), 
-      RTX_2060 = addIVA(142000), 
-      RTX_3060_Ti = addIVA(205000), 
-      RTX_3080 = addIVA(300000), 
-      RTX_3090 = addIVA(410000); 
+const GTX_1660 = addIVA(120000),
+  RTX_2060 = addIVA(142000),
+  RTX_3060_Ti = addIVA(205000),
+  RTX_3080 = addIVA(300000),
+  RTX_3090 = addIVA(410000);
 
 //Precio de los procesadores Intel
 const I5_10400f = addIVA(41000),
-      I7_10700 = addIVA(80000),
-      I9_11900k = addIVA(153000);
+  I7_10700 = addIVA(80000),
+  I9_11900k = addIVA(153000);
 
 //Precio de los procesadores AMD
 const Ryzen_5600x = addIVA(73000),
-      Ryzen_5700g = addIVA(82500);
+  Ryzen_5700g = addIVA(82500);
 
 //Precio de las RAMs
 const Fury_8GB = addIVA(16700),
-      Fury_4GB = addIVA(9400),
-      Vengance_8GB = addIVA(16000);
+  Fury_4GB = addIVA(9400),
+  Vengance_8GB = addIVA(16000);
 
 //Precio de los Cases
 const Naxido_F300 = addIVA(19300),
-      Brainstorm_Mastra = addIVA(26000),
-      Solarmax_Cm5901 = addIVA(22000),
-      Sentey_K20 = addIVA(29000);
-
+  Brainstorm_Mastra = addIVA(26000),
+  Solarmax_Cm5901 = addIVA(22000),
+  Sentey_K20 = addIVA(29000);
 
 let discount = 0; //Los codigos de descuento son "descuento 20" y "descuento 50"
-let menu, prod, section, total = 0, value = 0;
+let menu,
+  prod,
+  section,
+  total = 0,
+  value = 0;
 
-menu = parseInt(prompt('A continuación pulse... \n 1.Acceder con código de descuento \n 2.Listado de productos'));
+menu = parseInt(
+  prompt(
+    "A continuación pulse... \n 1.Acceder con código de descuento \n 2.Listado de productos"
+  )
+);
 
-while(menu !== 1 && menu !== 2){
-    datoInvalido();
+while (menu !== 1 && menu !== 2) {
+  datoInvalido();
 
-    menu = parseInt(prompt('Por favor, seleccione una de las opciones:\n1.Acceder con código de descuento \n2.Listado de productos'));
+  menu = parseInt(
+    prompt(
+      "Por favor, seleccione una de las opciones:\n1.Acceder con código de descuento \n2.Listado de productos"
+    )
+  );
 }
 
 console.log('Los códigos de descuento son "descuento 20" y "descuento 50"');
 
-if(menu === 1){
-    discount = prompt("Ingrese el código de descuento para recibirlo o introduzca ESC para salir");
+if (menu === 1) {
+  discount = prompt(
+    "Ingrese el código de descuento para recibirlo o introduzca ESC para salir"
+  );
 
-    while((discount !== 'ESC') && (discount !== 'descuento 20') && (discount !== 'descuento 50')){
-        datoInvalido();
+  while (
+    discount !== "ESC" &&
+    discount !== "descuento 20" &&
+    discount !== "descuento 50"
+  ) {
+    datoInvalido();
 
-        discount = prompt("Por favor, ingrese el código de descuento para recibirlo o introduzca ESC para salir");
-    }
+    discount = prompt(
+      "Por favor, ingrese el código de descuento para recibirlo o introduzca ESC para salir"
+    );
+  }
 
-    if(discount === 'ESC'){ discount = 0 }
-
-    else if(discount === 'descuento 20'){ discount = 0.20 }
-
-    else{ discount = 0.50 }
+  if (discount === "ESC") {
+    discount = 0;
+  } else if (discount === "descuento 20") {
+    discount = 0.2;
+  } else {
+    discount = 0.5;
+  }
 }
 
-do{
-    section = parseInt(prompt('Seleccione un categoría: \n1.Tarjeta gráfica \n2.Procesador \n3.Memoria RAM \n4.Gabinete \n0.Cerrar su compra'));
+do {
+  section = parseInt(
+    prompt(
+      "Seleccione un categoría: \n1.Tarjeta gráfica \n2.Procesador \n3.Memoria RAM \n4.Gabinete \n0.Cerrar su compra"
+    )
+  );
 
-    while(section !== 1 && section !== 2 && section !== 3 && section !== 4 && section !== 0){
-        datoInvalido();
+  while (
+    section !== 1 &&
+    section !== 2 &&
+    section !== 3 &&
+    section !== 4 &&
+    section !== 0
+  ) {
+    datoInvalido();
 
-        section= parseInt(prompt('Por favor, vuelva a seleccionar una: \n1.Tarjeta gráfica \n2.Procesador \n3.Memoria RAM \n4.Gabinete \n0.Cerrar su compra'));
-    }
+    section = parseInt(
+      prompt(
+        "Por favor, vuelva a seleccionar una: \n1.Tarjeta gráfica \n2.Procesador \n3.Memoria RAM \n4.Gabinete \n0.Cerrar su compra"
+      )
+    );
+  }
 
-    switch(section){
-        case 1:
-            value = GPU_selection();
+  switch (section) {
+    case 1:
+      value = GPU_selection();
 
-            value = addDiscount(value);
-            total += value;
-            break;
+      value = addDiscount(value);
+      total += value;
+      break;
 
-        case 2:
-            value = CPU_selection();
+    case 2:
+      value = CPU_selection();
 
-            value = addDiscount(value);
-            total += value;
-            break;
-        
-        case 3:
-            value = RAM_selection();
+      value = addDiscount(value);
+      total += value;
+      break;
 
-            value = addDiscount(value);
-            total += value;
-            break;
-        
-        case 4:
-            value = Cases_selection();
+    case 3:
+      value = RAM_selection();
 
-            value = addDiscount(value);
-            total += value;
-            break;
-    }
+      value = addDiscount(value);
+      total += value;
+      break;
 
+    case 4:
+      value = Cases_selection();
 
-}while(section);
+      value = addDiscount(value);
+      total += value;
+      break;
+  }
+} while (section);
 
-alert('Usted ah de pagar ' + total);
+alert("Usted ah de pagar " + total);
 
-function GPU_selection(){
-    let price;
+function GPU_selection() {
+  let price;
 
-    prod = parseInt(prompt('Seleccione su artículo: \n1.GTX 1660 SUPER $' + GTX_1660
-                                                + '\n2.RTX 2060 $' + RTX_2060
-                                                + '\n3.RTX 3060 Ti $' + RTX_3060_Ti
-                                                + '\n4.RTX 3080 $' + RTX_3080
-                                                + '\n5.RTX 3090 $' + RTX_3090));
-    
-    while(prod !== 1 && prod !== 2 && prod !== 3 && prod !== 4 && prod !== 5){
-        datoInvalido();
+  prod = parseInt(
+    prompt(
+      "Seleccione su artículo: \n1.GTX 1660 SUPER $" +
+        GTX_1660 +
+        "\n2.RTX 2060 $" +
+        RTX_2060 +
+        "\n3.RTX 3060 Ti $" +
+        RTX_3060_Ti +
+        "\n4.RTX 3080 $" +
+        RTX_3080 +
+        "\n5.RTX 3090 $" +
+        RTX_3090
+    )
+  );
 
-        prod = parseInt(prompt('Seleccione su artículo: \n1.GTX 1660 SUPER $' + GTX_1660
-                                                     + '\n2.RTX 2060 $' + RTX_2060
-                                                     + '\n3.RTX 3060 Ti $' + RTX_3060_Ti
-                                                     + '\n4.RTX 3080 $' + RTX_3080
-                                                     + '\n5.RTX 3090 $' + RTX_3090));
-    }
+  while (prod !== 1 && prod !== 2 && prod !== 3 && prod !== 4 && prod !== 5) {
+    datoInvalido();
 
-    switch(prod){
-        case 1: 
-            price = GTX_1660;
-            break;
+    prod = parseInt(
+      prompt(
+        "Seleccione su artículo: \n1.GTX 1660 SUPER $" +
+          GTX_1660 +
+          "\n2.RTX 2060 $" +
+          RTX_2060 +
+          "\n3.RTX 3060 Ti $" +
+          RTX_3060_Ti +
+          "\n4.RTX 3080 $" +
+          RTX_3080 +
+          "\n5.RTX 3090 $" +
+          RTX_3090
+      )
+    );
+  }
 
-        case 2:
-            price = RTX_2060;
-            break;
+  switch (prod) {
+    case 1:
+      price = GTX_1660;
+      break;
 
-        case 3:
-            price = RTX_3060_Ti;
-            break;
+    case 2:
+      price = RTX_2060;
+      break;
 
-        case 4:
-            price = RTX_3080;
-            break;
+    case 3:
+      price = RTX_3060_Ti;
+      break;
 
-        case 5:
-            price = RTX_3090;
-            break;
-    }
+    case 4:
+      price = RTX_3080;
+      break;
 
-    return price;
+    case 5:
+      price = RTX_3090;
+      break;
+  }
+
+  return price;
 }
 
+function CPU_selection() {
+  let price;
 
-function CPU_selection(){
-    let price;
+  prod = parseInt(
+    prompt(
+      "Seleccione su artículo: \n1.Intel I5 10400f $" +
+        I5_10400f +
+        "\n2.Intel I7 10700 $" +
+        I7_10700 +
+        "\n3.Intel I9 9 Ti $" +
+        I9_11900k +
+        "\n4.AMD Ryzen 5600X $" +
+        Ryzen_5600x +
+        "\n5.AMD Ryzen 5700g $" +
+        Ryzen_5700g
+    )
+  );
 
-    prod = parseInt(prompt('Seleccione su artículo: \n1.Intel I5 10400f $' + I5_10400f  
-                                                + '\n2.Intel I7 10700 $' + I7_10700
-                                                + '\n3.Intel I9 9 Ti $' + I9_11900k
-                                                + '\n4.AMD Ryzen 5600X $' + Ryzen_5600x
-                                                + '\n5.AMD Ryzen 5700g $' + Ryzen_5700g));
-    
-    while(prod !== 1 && prod !== 2 && prod !== 3 && prod !== 4 && prod !== 5){
-        datoInvalido();
+  while (prod !== 1 && prod !== 2 && prod !== 3 && prod !== 4 && prod !== 5) {
+    datoInvalido();
 
-        prod = parseInt(prompt('Seleccione su artículo: \n1.Intel I5 10400f $' + I5_10400f  
-                                                    + '\n2.Intel I7 10700 $' + I7_10700
-                                                    + '\n3.Intel I9 9 Ti $' + I9_11900k
-                                                    + '\n4.AMD Ryzen 5600X $' + Ryzen_5600x
-                                                    + '\n5.AMD Ryzen 5700g $' + Ryzen_5700g));
-    }
+    prod = parseInt(
+      prompt(
+        "Seleccione su artículo: \n1.Intel I5 10400f $" +
+          I5_10400f +
+          "\n2.Intel I7 10700 $" +
+          I7_10700 +
+          "\n3.Intel I9 9 Ti $" +
+          I9_11900k +
+          "\n4.AMD Ryzen 5600X $" +
+          Ryzen_5600x +
+          "\n5.AMD Ryzen 5700g $" +
+          Ryzen_5700g
+      )
+    );
+  }
 
-    switch(prod){
-        case 1: 
-            price = I5_10400f;
-            break;
+  switch (prod) {
+    case 1:
+      price = I5_10400f;
+      break;
 
-        case 2:
-            price = I7_10700;
-            break;
+    case 2:
+      price = I7_10700;
+      break;
 
-        case 3:
-            price = I9_11900k;
-            break;
+    case 3:
+      price = I9_11900k;
+      break;
 
-        case 4:
-            price = Ryzen_5600x;
-            break;
+    case 4:
+      price = Ryzen_5600x;
+      break;
 
-        case 5:
-            price = Ryzen_5700g;
-            break;
-    }
+    case 5:
+      price = Ryzen_5700g;
+      break;
+  }
 
-    return price;
+  return price;
 }
-
 
 function RAM_selection() {
-    let price;
+  let price;
 
-    prod = parseInt(prompt('Seleccione su artículo: \n1.Kingston Fury RGB 8GB $' + Fury_8GB  
-                                                + '\n2.Kingston Fury 4GB $' + Fury_4GB  
-                                                + '\n3.Corsair Vengance RGB 8GB $' + Vengance_8GB));
-    
-    while(prod !== 1 && prod !== 2 && prod !== 3){
-        datoInvalido();
-        
-        prod = parseInt(prompt('Seleccione su artículo: \n1.Kingston Fury RGB 8GB $' + Fury_8GB  
-                                                    + '\n2.Kingston Fury 4GB $' + Fury_4GB  
-                                                    + '\n3.Corsair Vengance RGB 8GB $' + Vengance_8GB));
+  prod = parseInt(
+    prompt(
+      "Seleccione su artículo: \n1.Kingston Fury RGB 8GB $" +
+        Fury_8GB +
+        "\n2.Kingston Fury 4GB $" +
+        Fury_4GB +
+        "\n3.Corsair Vengance RGB 8GB $" +
+        Vengance_8GB
+    )
+  );
 
-    }
+  while (prod !== 1 && prod !== 2 && prod !== 3) {
+    datoInvalido();
 
-    switch(prod){
-        case 1: 
-            price = Fury_8GB;
-            break;
+    prod = parseInt(
+      prompt(
+        "Seleccione su artículo: \n1.Kingston Fury RGB 8GB $" +
+          Fury_8GB +
+          "\n2.Kingston Fury 4GB $" +
+          Fury_4GB +
+          "\n3.Corsair Vengance RGB 8GB $" +
+          Vengance_8GB
+      )
+    );
+  }
 
-        case 2:
-            price = Fury_4GB;
-            break;
+  switch (prod) {
+    case 1:
+      price = Fury_8GB;
+      break;
 
-        case 3:
-            price = Vengance_8GB;
-            break;
-    }
-    
-    return price;
+    case 2:
+      price = Fury_4GB;
+      break;
+
+    case 3:
+      price = Vengance_8GB;
+      break;
+  }
+
+  return price;
 }
 
+function Cases_selection() {
+  let price;
+  prod = parseInt(
+    prompt(
+      "Seleccione su artículo: \n1.Naxido F300 $" +
+        Naxido_F300 +
+        "\n2.Brainstorm Mastra $" +
+        Brainstorm_Mastra +
+        "\n3.Solarmax_Cm5901 $" +
+        Solarmax_Cm5901 +
+        "\n4.Sentey K20 $" +
+        Sentey_K20
+    )
+  );
 
-function Cases_selection(){
-    let price
-    prod = parseInt(prompt('Seleccione su artículo: \n1.Naxido F300 $' + Naxido_F300  
-                                                + '\n2.Brainstorm Mastra $' + Brainstorm_Mastra 
-                                                + '\n3.Solarmax_Cm5901 $' + Solarmax_Cm5901
-                                                + '\n4.Sentey K20 $' + Sentey_K20));
-    
-    while(prod !== 1 && prod !== 2 && prod !== 3 && prod !== 4 && prod !== 5){
-        datoInvalido();
+  while (prod !== 1 && prod !== 2 && prod !== 3 && prod !== 4 && prod !== 5) {
+    datoInvalido();
 
-        prod = parseInt(prompt('Seleccione su artículo: \n1.Naxido F300 $' + Naxido_F300  
-                                                    + '\n2.Brainstorm Mastra $' + Brainstorm_Mastra 
-                                                    + '\n3.Solarmax_Cm5901 $' + Solarmax_Cm5901
-                                                    + '\n4.Sentey K20 $' + Sentey_K20));
-    }
+    prod = parseInt(
+      prompt(
+        "Seleccione su artículo: \n1.Naxido F300 $" +
+          Naxido_F300 +
+          "\n2.Brainstorm Mastra $" +
+          Brainstorm_Mastra +
+          "\n3.Solarmax_Cm5901 $" +
+          Solarmax_Cm5901 +
+          "\n4.Sentey K20 $" +
+          Sentey_K20
+      )
+    );
+  }
 
-    switch(prod){
-        case 1: 
-            price = Naxido_F300;
-            break;
+  switch (prod) {
+    case 1:
+      price = Naxido_F300;
+      break;
 
-        case 2:
-            price = Brainstorm_Mastra;
-            break;
+    case 2:
+      price = Brainstorm_Mastra;
+      break;
 
-        case 3:
-            price = Solarmax_Cm5901;
-            break;
+    case 3:
+      price = Solarmax_Cm5901;
+      break;
 
-        case 4:
-            price = Sentey_K20;
-            break;
-    }
+    case 4:
+      price = Sentey_K20;
+      break;
+  }
 
-    return price;
+  return price;
 }
---
+++ PreEntrega 2 Gesualdi/js/functions.js
@@ -0,0 +1,149 @@
+const iva = 21, //Impuesto al Valor Agregado
+  addIva = (priceWhitoutIva) => priceWhitoutIva + (priceWhitoutIva * iva) / 100,
+
+  addDiscount = (price, discount) => price - (price * discount) / 100,
+
+  invalid = () => alert("EL DATO INGRESADO NO FUE VALIDO"),
+
+  loopChecker = (array) =>
+    array[0] % 1 !== 0 || array[0] < 0 || (array[0] > 0 && array[0] > array[1]), 
+
+  printPriceARG = (price) =>
+    "$" + new Intl.NumberFormat("es-AR", {}).format(price),
+
+  getRapidInfo = (element) =>
+    element.brand + " " + element.name + " " + printPriceARG(element.price),
+  
+  getCategories = (selection) => {
+    switch (selection) {
+      case 1:
+        return "Tarjetas Gráficas";
+
+      case 2:
+        return "Procesadores de Escritorio";
+
+      case 3:
+        return "Memorias RAM";
+
+      case 4:
+        return "Gabinetes de Escritorio";
+    }
+  },
+  
+  printCategories = (array) => {
+    return (
+      "1. " +
+      [...new Set(
+          array.map((el) => el.category)
+        ),
+      ].reduce((acc, el, inx) => (acc = acc + "\n" + (inx + 1) + ". " + el))
+    );
+  },
+  
+  menuSelection = (array) => {
+    const options = printCategories(array);
+
+    return [
+      parseFloat(
+        prompt(
+          "Seleccione una de las categorias: \n" +
+            "0. Finalizar compra \n" +
+            options
+        )
+      ),
+      options.split("\n").length,
+    ];
+  },
+
+  productsSelector = (array, categoryInx) => {
+    const options = array
+      .filter((el) => el.category === getCategories(categoryInx))
+      .map((el) => getRapidInfo(el));
+
+    const userChoice = parseFloat(
+      prompt(
+        "Seleccione un articulo indicando su índice o 0 para regresar: \n" +
+          options.join("\n")
+      )
+    );
+
+    return [userChoice, options.length];
+  },
+
+  infoPreview = (array, categoryInx, productInx) => {
+    let newArray = Object.entries(
+      array.filter((el) => el.category === getCategories(categoryInx))[
+        productInx - 1
+      ]
+    ).filter(
+      (el) => el[1] !== false && el[0] !== "category" && el[1] !== undefined
+    );
+
+    console.log(newArray);
+
+    newArray[0][1] = printPriceARG(newArray[0][1]);
+    return parseFloat(
+      prompt(
+        "Pulse: 1.Continuar ///" +
+          "/// 0.Regresar \n" +
+          newArray.join("\n").split(",").join(": ")
+      )
+    );
+  },
+
+  addToCart = (array, categoryInx, productInx) => {
+    return array.filter((el) => el.category === getCategories(categoryInx))[
+      productInx - 1
+    ];
+  },
+
+  getDiscount = (array, discount) => {
+    return addDiscount(
+      array.reduce((acc, el) => acc + el.price, 0),
+      discount
+    );
+  },
+
+  discountCodesChecker = (array, userSelection) => {
+    if (userSelection === "descuento 20") return getDiscount(array, 20);
+
+    else if (userSelection === "descuento 50") return getDiscount(array, 50);
+
+    else if (userSelection === "0") return getDiscount(array, 0);
+
+    else return -1;
+  },
+
+  discountCaller = (array) => {
+    const userDiscountSelector = prompt(
+      "Si quiere utilizar un codigo de descuento introduzcalo \n" +
+        "En caso contrario introduzca 0"
+    );
+    return discountCodesChecker(array, userDiscountSelector);
+  },
+
+  printCart = (array, price) => {
+    let cart;
+
+    cart = array.map((el) => getRapidInfo(el)).join("\n");
+
+    alert(
+      "Su carrito es: \n" +
+        cart +
+        "\n" +
+        "Siendo el TOTAL: " +
+        printPriceARG(price)
+    );
+  };
+
+export {
+  addIva,
+  invalid,
+  loopChecker,
+  menuSelection,
+  productsSelector,
+  infoPreview,
+  addToCart,
+  discountCaller,
+  printCart,
+};
